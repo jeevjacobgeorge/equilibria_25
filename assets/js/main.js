e89611048eefa30154bfe4c13e98db080d03ec5b
jeevjacobgeorge <jeev@J4Hp>
2024-10-13 21:31:24 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,113 @@
+/*
+ * Change Navbar color while scrolling
+*/
+
+$(window).scroll(function(){
+	handleTopNavAnimation();
+});
+
+$(window).load(function(){
+	handleTopNavAnimation();
+});
+
+function handleTopNavAnimation() {
+	var top=$(window).scrollTop();
+
+	if(top>10){
+		$('#site-nav').addClass('navbar-solid'); 
+	}
+	else{
+		$('#site-nav').removeClass('navbar-solid'); 
+	}
+}
+
+/*
+ * Registration Form
+*/
+
+$('#registration-form').submit(function(e){
+    e.preventDefault();
+    
+    var postForm = { //Fetch form data
+            'fname'     : $('#registration-form #fname').val(),
+            'lname'     : $('#registration-form #lname').val(),
+            'email'     : $('#registration-form #email').val(),
+            'cell'      : $('#registration-form #cell').val(),
+            'address'   : $('#registration-form #address').val(),
+            'zip'       : $('#registration-form #zip').val(),
+            'city'      : $('#registration-form #city').val(),
+            'program'   : $('#registration-form #program').val()
+    };
+
+    $.ajax({
+            type      : 'POST',
+            url       : './assets/php/contact.php',
+            data      : postForm,
+            dataType  : 'json',
+            success   : function(data) {
+                            if (data.success) {
+                                $('#registration-msg .alert').html("Registration Successful");
+                                $('#registration-msg .alert').removeClass("alert-danger");
+                                $('#registration-msg .alert').addClass("alert-success");
+                                $('#registration-msg').show();
+                            }
+                            else
+                            {
+                                $('#registration-msg .alert').html("Registration Failed");
+                                $('#registration-msg .alert').removeClass("alert-success");
+                                $('#registration-msg .alert').addClass("alert-danger");
+                                $('#registration-msg').show();
+                            }
+                        }
+        });
+});
+
+/*
+ * SmoothScroll
+*/
+
+smoothScroll.init();
+
+(function () {
+    const second = 1000,
+          minute = second * 60,
+          hour = minute * 60,
+          day = hour * 24;
+  
+    //I'm adding this section so I don't have to keep updating this pen every year :-)
+    //remove this if you don't need it
+    let today = new Date(),
+        dd = String(today.getDate()).padStart(2, "0"),
+        mm = String(today.getMonth() + 1).padStart(2, "0"),
+        yyyy = today.getFullYear(),
+        nextYear = yyyy + 1,
+        dayMonth = "01/24/",
+        event_day = dayMonth + yyyy;
+    
+    today = mm + "/" + dd + "/" + yyyy;
+    if (today > event_day) {
+      event_day = dayMonth + nextYear;
+    }
+    //end
+    
+    const countDown = new Date(event_day).getTime(),
+        x = setInterval(function() {    
+  
+          const now = new Date().getTime(),
+                distance = countDown - now;
+  
+          document.getElementById("days").innerText = Math.floor(distance / (day)),
+            document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+            document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+            document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+  
+          //do something later when date is reached
+          if (distance < 0) {
+            document.getElementById("headline").innerText = "Event has started!";
+            document.getElementById("countdown").style.display = "none";
+            document.getElementById("content").style.display = "block";
+            clearInterval(x);
+          }
+          //seconds
+        }, 0)
+    }());
